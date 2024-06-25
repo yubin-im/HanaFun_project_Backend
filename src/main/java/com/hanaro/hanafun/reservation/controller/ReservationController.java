84b3cb66452ffede31e567eeae40d6a90098b51b
yubin-im <ybyb130@gmail.com>
2024-06-25 15:42:05 +0900

    feat: 마이페이지 데이터 출력(예약내역) 기능 구현

--- src/main/java/com/hanaro/hanafun/reservation/controller/ReservationController.java
+++ src/main/java/com/hanaro/hanafun/reservation/controller/ReservationController.java
@@ -1,7 +1,11 @@
 package com.hanaro.hanafun.reservation.controller;
 
+import com.hanaro.hanafun.reservation.dto.request.MyPageReqDto;
+import com.hanaro.hanafun.reservation.dto.response.MyPageResDto;
 import com.hanaro.hanafun.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationController {
     private final ReservationService reservationService;
 
+    @GetMapping("/reservation/my")
+    public MyPageResDto myPage(@RequestBody MyPageReqDto myPageReqDto) {
+        return reservationService.myPage(myPageReqDto);
+    }
 }
