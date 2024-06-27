691b16361b140a698a2818081b431c709347a4e7
yubin-im <ybyb130@gmail.com>
2024-06-27 13:53:51 +0900

    feat: 클래스 예약 가능 날짜 출력 기능 구현

--- src/main/java/com/hanaro/hanafun/lessondate/service/impl/LessonDateServiceImpl.java
+++ src/main/java/com/hanaro/hanafun/lessondate/service/impl/LessonDateServiceImpl.java
@@ -1,7 +1,10 @@
 package com.hanaro.hanafun.lessondate.service.impl;
 
+import com.hanaro.hanafun.lesson.domain.LessonEntity;
 import com.hanaro.hanafun.lessondate.domain.LessonDateEntity;
 import com.hanaro.hanafun.lessondate.domain.LessonDateRepository;
+import com.hanaro.hanafun.lessondate.dto.request.AvailableDateReqDto;
+import com.hanaro.hanafun.lessondate.dto.response.AvailableDateResDto;
 import com.hanaro.hanafun.lessondate.dto.response.LessonDetailResDto;
 import com.hanaro.hanafun.lessondate.service.LessonDateService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -35,5 +39,35 @@ public class LessonDateServiceImpl implements LessonDateService {
                 .collect(Collectors.toList());
 
         return lessonDetails;
+    }
+
+    // 클래스 예약 가능 날짜 출력
+    @Transactional
+    @Override
+    public List<AvailableDateResDto> availableDate(AvailableDateReqDto availableDateReqDto) {
+        List<LessonDateEntity> lessonDates = lessonDateRepository.findLessonDateEntitiesByLessonEntity_LessonId(availableDateReqDto.getLessonId());
+
+        List<AvailableDateResDto> availableDateResDtos = lessonDates.stream()
+                .map(lessonDate -> {
+                    LessonEntity lesson = lessonDate.getLessonEntity();
+                    int quantityLeft = lesson.getCapacity() - lessonDate.getApplicant();
+
+                    // 잔여 수량 0 이하 날짜 필터링
+                    if (quantityLeft <= 0) {
+                        return null;
+                    }
+
+                    return AvailableDateResDto.builder()
+                            .lessondateId(lessonDate.getLessondateId())
+                            .date(lessonDate.getDate())
+                            .startTime(lessonDate.getStartTime())
+                            .endTime(lessonDate.getEndTime())
+                            .quantityLeft(quantityLeft)
+                            .build();
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        return availableDateResDtos;
     }
 }
