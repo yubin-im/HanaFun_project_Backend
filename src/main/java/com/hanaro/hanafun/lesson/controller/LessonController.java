5fb900ea1de25a6990307fe328a51b75c4ed8449
yubin-im <ybyb130@gmail.com>
2024-06-27 15:17:39 +0900

    feat: S3 이미지 업로드 기능 구현

--- src/main/java/com/hanaro/hanafun/lesson/controller/LessonController.java
+++ src/main/java/com/hanaro/hanafun/lesson/controller/LessonController.java
@@ -5,15 +5,15 @@ import com.hanaro.hanafun.lesson.dto.request.OpenedLessonsReqDto;
 import com.hanaro.hanafun.lesson.dto.response.LessonInfoResDto;
 import com.hanaro.hanafun.lesson.dto.response.OpenedLessonsResDto;
 import com.hanaro.hanafun.lesson.service.LessonService;
+import com.hanaro.hanafun.lesson.service.S3UploadService;
 import com.hanaro.hanafun.lessondate.dto.response.LessonDetailResDto;
 import com.hanaro.hanafun.lessondate.service.LessonDateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -21,6 +21,7 @@ import java.util.List;
 public class LessonController {
     private final LessonService lessonService;
     private final LessonDateService lessonDateService;
+    private final S3UploadService s3UploadService;
 
     // 개설 클래스 관리- 개설 클래스 목록 출력
     @GetMapping("/reservation/my/opened")
@@ -41,5 +42,16 @@ public class LessonController {
     public ResponseEntity<ApiResponse> lessonInfo(@PathVariable Long lessonId) {
         LessonInfoResDto lessonInfoResDto = lessonService.lessonInfo(lessonId);
         return ResponseEntity.ok(new ApiResponse<>(true, "ok", lessonInfoResDto));
+    }
+
+    // 클래스 이미지 업로드
+    @PostMapping("/lesson/image-upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+            String imageUrl = s3UploadService.saveFile(file);
+            return ResponseEntity.ok(imageUrl);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("파일 업로드 중 오류가 발생했습니다.");
+        }
     }
 }
