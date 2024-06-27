d4ec7a0637654753984218936c4c32a13f8d1305
doSeung11 <dounts706@naver.com>
2024-06-27 11:10:26 +0900

    feat: host info

--- src/main/java/com/hanaro/hanafun/lesson/domain/LessonRepository.java
+++ src/main/java/com/hanaro/hanafun/lesson/domain/LessonRepository.java
@@ -10,6 +10,5 @@ import java.util.Optional;
 @Repository
 public interface LessonRepository extends JpaRepository<LessonEntity, Long> {
     List<LessonEntity> findLessonEntitiesByHostEntity(HostEntity hostEntity);
-
     Optional<List<LessonEntity>> findByHostEntityHostId(Long hostId);
 }
