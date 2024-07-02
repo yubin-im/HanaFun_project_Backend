5258fe13dd057d9ddc5804a90cba8b015bf5ccca
doSeung11 <dounts706@naver.com>
2024-07-02 16:57:22 +0900

    feat: pw check api

--- src/main/java/com/hanaro/hanafun/account/controller/AccountController.java
+++ src/main/java/com/hanaro/hanafun/account/controller/AccountController.java
@@ -1,12 +1,15 @@
 package com.hanaro.hanafun.account.controller;
 
 import com.hanaro.hanafun.account.dto.AccountResDto;
+import com.hanaro.hanafun.account.dto.PwReqDto;
+import com.hanaro.hanafun.account.dto.PwResDto;
 import com.hanaro.hanafun.account.service.impl.AccountServiceImpl;
 import com.hanaro.hanafun.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,11 @@ public class AccountController {
     ResponseEntity<ApiResponse> readAccountList(@AuthenticationPrincipal Long userId){
         List<AccountResDto> accountResDtoList = accountService.readAccountList(userId);
         return ResponseEntity.ok(new ApiResponse(true, "ok", accountResDtoList));
+    }
+
+    @GetMapping("/pw")
+    ResponseEntity<ApiResponse> checkAccountPw(@RequestBody PwReqDto pwReqDto){
+        PwResDto pwResDto = accountService.checkAccountPw(pwReqDto);
+        return ResponseEntity.ok(new ApiResponse(true, "ok", pwResDto));
     }
 }
