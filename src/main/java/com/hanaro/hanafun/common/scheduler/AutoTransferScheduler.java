2333b81c31c445d1a2b4fdde2cdbf0acfe8bd49a
doSeung11 <dounts706@naver.com>
2024-07-03 13:00:40 +0900

    scheduling testing

--- src/main/java/com/hanaro/hanafun/common/scheduler/AutoTransferScheduler.java
+++ src/main/java/com/hanaro/hanafun/common/scheduler/AutoTransferScheduler.java
@@ -2,6 +2,8 @@ package com.hanaro.hanafun.common.scheduler;
 
 import com.hanaro.hanafun.transaction.service.impl.TransactionServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +12,11 @@ import org.springframework.stereotype.Component;
 public class AutoTransferScheduler {
     private final TransactionServiceImpl transactionService;
 
-    @Scheduled(cron = "0 0 0 * * ?") //매일 0시
+    private static final Logger logger = LoggerFactory.getLogger(AutoTransferScheduler.class);
+
+    @Scheduled(cron = "0 15 13 * * ?") //매일 0시
     public void runAutoTransfer(){
+        logger.info("auto transfer scheduling test started!!!!!!!!!!!!!!");
         transactionService.autoTransfer();
     }
 }
