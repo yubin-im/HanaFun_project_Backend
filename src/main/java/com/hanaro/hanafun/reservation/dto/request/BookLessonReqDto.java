0e59dc46f8238157238328555ab1c05913f458dd
yubin-im <ybyb130@gmail.com>
2024-07-03 14:17:08 +0900

    feat: 클래스 예약 userId-> 토큰 받아오는 것으로 수정

--- src/main/java/com/hanaro/hanafun/reservation/dto/request/BookLessonReqDto.java
+++ src/main/java/com/hanaro/hanafun/reservation/dto/request/BookLessonReqDto.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 @Getter
 public class BookLessonReqDto {
     private Long lessondateId;
-    private Long userId;
     private int applicant;  // 예약 수량
     private Long accountId;
     private String password;  // 계좌 비밀번호 확인
